90b0c1d23a331d949b9af7597c1385412ee762b3
brenner <fabian.brenner@sit.fraunhofer.de>
2019-09-10 13:05:29 +0200

    added Assert for typingList-Content for testHugeCommonTypingPair-Test #1

--- src/test/java/soot/jimple/toolkits/typing/TypingMinimizeTest.java
+++ src/test/java/soot/jimple/toolkits/typing/TypingMinimizeTest.java
@@ -345,60 +345,58 @@ public class TypingMinimizeTest {
     Local x2 = new JimpleLocal("$x2", null);
     Local x3 = new JimpleLocal("$x3", null);
 
-    Typing typing = new Typing(Arrays.asList(x1, x2, x3));
-    typing.set(x1, Type1);
-    typing.set(x2, Type1);
-    typing.set(x3, Type1);
-    typingList.add(typing);
+    Typing typing1 = new Typing(Arrays.asList(x1, x2, x3));
+    typing1.set(x1, Type1);
+    typing1.set(x2, Type1);
+    typing1.set(x3, Type1);
+    typingList.add(typing1);
 
-    typing = new Typing(Arrays.asList(x1, x2, x3));
-    typing.set(x1, Type2);
-    typing.set(x2, Type1);
-    typing.set(x3, Type1);
-    typingList.add(typing);
+    Typing typing2 = new Typing(Arrays.asList(x1, x2, x3));
+    typing2.set(x1, Type2);
+    typing2.set(x2, Type1);
+    typing2.set(x3, Type1);
+    typingList.add(typing2);
 
-    typing = new Typing(Arrays.asList(x1, x2, x3));
-    typing.set(x1, Type1);
-    typing.set(x2, Type2);
-    typing.set(x3, Type1);
-    typingList.add(typing);
+    Typing typing3 = new Typing(Arrays.asList(x1, x2, x3));
+    typing3.set(x1, Type1);
+    typing3.set(x2, Type2);
+    typing3.set(x3, Type1);
+    typingList.add(typing3);
 
-    typing = new Typing(Arrays.asList(x1, x2, x3));
-    typing.set(x1, Type1);
-    typing.set(x2, Type1);
-    typing.set(x3, Type2);
-    typingList.add(typing);
+    Typing typing4 = new Typing(Arrays.asList(x1, x2, x3));
+    typing4.set(x1, Type1);
+    typing4.set(x2, Type1);
+    typing4.set(x3, Type2);
+    typingList.add(typing4);
 
-    typing = new Typing(Arrays.asList(x1, x2, x3));
-    typing.set(x1, Type2);
-    typing.set(x2, Type2);
-    typing.set(x3, Type1);
-    typingList.add(typing);
+    Typing typing5 = new Typing(Arrays.asList(x1, x2, x3));
+    typing5.set(x1, Type2);
+    typing5.set(x2, Type2);
+    typing5.set(x3, Type1);
+    typingList.add(typing5);
 
-    typing = new Typing(Arrays.asList(x1, x2, x3));
-    typing.set(x1, Type2);
-    typing.set(x2, Type1);
-    typing.set(x3, Type2);
-    typingList.add(typing);
+    Typing typing6 = new Typing(Arrays.asList(x1, x2, x3));
+    typing6.set(x1, Type2);
+    typing6.set(x2, Type1);
+    typing6.set(x3, Type2);
+    typingList.add(typing6);
 
-    typing = new Typing(Arrays.asList(x1, x2, x3));
-    typing.set(x1, Type1);
-    typing.set(x2, Type2);
-    typing.set(x3, Type2);
-    typingList.add(typing);
+    Typing typing7 = new Typing(Arrays.asList(x1, x2, x3));
+    typing7.set(x1, Type1);
+    typing7.set(x2, Type2);
+    typing7.set(x3, Type2);
+    typingList.add(typing7);
 
-    typing = new Typing(Arrays.asList(x1, x2, x3));
-    typing.set(x1, Type2);
-    typing.set(x2, Type2);
-    typing.set(x3, Type2);
-    typingList.add(typing);
+    Typing typing8 = new Typing(Arrays.asList(x1, x2, x3));
+    typing8.set(x1, Type2);
+    typing8.set(x2, Type2);
+    typing8.set(x3, Type2);
+    typingList.add(typing8);
 
     Typing.minimize(typingList, new BytecodeHierarchy());
 
-    assertEquals(8, typingList.size());
-    
-    TODO
-    // assertThat(typingList, containsInAnyOrder(typing2, typing3));
+    assertEquals(8, typingList.size()); 
+    assertThat(typingList, containsInAnyOrder(typing1, typing2, typing3, typing4, typing5, typing6, typing7, typing8));
   }
 
   @Test
